5410c37b3521f8a774fd844656b0724c5bb5220e
estebanquintana6 <a01184855@itesm.mx>
2018-05-02 18:44:39 -0500

    Done

--- methods/src/Person.java
+++ methods/src/Person.java
@@ -3,9 +3,16 @@ import java.util.Random;
 public class Person extends Thread {
 
     int id;
+    Window w;
 
-    public Person(int id){
+
+    public Person(int id, Window w){
         this.id = id;
+        this.w = w;
+    }
+
+    public void setW(Window w) {
+        this.w = w;
     }
 
     @Override
@@ -14,10 +21,11 @@ public class Person extends Thread {
         Random random = new Random();
 
         int randomTime = random.nextInt(3000 - 1000 + 1) + 1000;
-        int randOutDoor;
 
         try {
+            System.out.println("Person with id: " + this.id + " getting atteded at window: " + w.getId());
             Thread.sleep(randomTime);
+            w.setBusy(false);
         } catch (InterruptedException e1) {
             e1.printStackTrace();
         }
--- methods/src/Queue.java
+++ methods/src/Queue.java
@@ -1,11 +1,14 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class Queue extends Thread{
+public class Queue{
     int secondsOpen;
     int windowNumber;
 
     List<Window> winList = new ArrayList<Window>();
+
+    java.util.Queue<Person> clientQueue = new LinkedList<>();
+
+    int clientsToday = 0;
 
     public Queue(int secondsOpen, int windowNumber){
         this.secondsOpen = secondsOpen;
@@ -17,14 +20,51 @@ public class Queue extends Thread{
         }
     }
 
-    @Override
     public void run() {
 
         long t= System.currentTimeMillis();
         long end = t+(secondsOpen*1000);
 
         while(System.currentTimeMillis() < end) {
-            
+            Random random = new Random();
+            int rand = random.nextInt(20 - 1 + 1) + 1;
+            if(rand == 1) {
+                Person person = new Person(clientsToday, null);
+                clientsToday++;
+                clientQueue.add(person);
+            }
+
+            int freeWindow = consultFreeWindow();
+
+            if(freeWindow != -1 && !clientQueue.isEmpty()){
+                Person personInTurn = clientQueue.remove();
+                Window w = winList.get(freeWindow);
+                personInTurn.setW(w);
+                w.setBusy(true);
+
+                Thread th = new Thread(personInTurn);
+                th.start();
+            }
         }
     }
+
+    public int consultFreeWindow(){
+
+        Iterator<Window> itr = winList.iterator();
+        while (itr.hasNext()) {
+            Window w = itr.next();
+            if(!w.isBusy()){
+                return w.getId();
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String args[]){
+        Queue service = new Queue(15, 4);
+        service.run();
+    }
 }
+
+
