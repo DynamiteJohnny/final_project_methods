79a8d4dc182af236d7522648d07f9f496fe55825
estebanquintana6 <a01184855@itesm.mx>
2018-05-03 00:59:27 -0500

    fucking kill me pls

--- methods/src/Queue.java
+++ methods/src/Queue.java
@@ -1,9 +1,11 @@
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class Queue{
     int secondsOpen;
     int windowNumber;
 
+    int miu;
 
     List<Window> winList = new ArrayList<Window>();
 
@@ -11,9 +13,10 @@ public class Queue{
 
     int clientsToday = 0;
 
-    public Queue(int secondsOpen, int windowNumber){
+    public Queue(int secondsOpen, int windowNumber, int miu){
         this.secondsOpen = secondsOpen;
         this.windowNumber = windowNumber;
+        this.miu = miu;
 
         for(int i = 0; i<windowNumber; i++){
             Window window = new Window(i);
@@ -21,20 +24,24 @@ public class Queue{
         }
     }
 
-    public void run() {
+    public void run() throws InterruptedException {
 
         long t= System.currentTimeMillis();
         long end = t+(secondsOpen*1000);
 
         while(System.currentTimeMillis() < end) {
             Random random = new Random();
-            int rand = random.nextInt(20 - 1 + 1) + 1;
 
-            if(rand == 1) {
-                Person person = new Person(clientsToday, null, System.currentTimeMillis());
-                clientsToday++;
-                clientQueue.add(person);
-            }
+            float low = (float) (miu-(miu*0.30));
+            float high = (float) (miu+(miu*0.30));
+
+            long rand = (long) (low + random.nextFloat() * (high - low));
+
+            TimeUnit.SECONDS.sleep(rand);
+
+            Person person = new Person(clientsToday, null, System.currentTimeMillis());
+            clientsToday++;
+            clientQueue.add(person);
 
             long attention_startTime = System.currentTimeMillis();
             int freeWindow = consultFreeWindow();
@@ -68,8 +75,12 @@ public class Queue{
         return -1;
     }
 
-    public static void main(String args[]){
-        Queue service = new Queue(15, 4);
+    public static void main(String args[]) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce miu(segundos) : ");
+        int miu = sc.nextInt();
+
+        Queue service = new Queue(15, 4, miu);
         service.run();
     }
 }
